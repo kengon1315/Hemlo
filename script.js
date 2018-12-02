2a16b0065b69a4c149c4832037650c8f352b04a8
kengon1315 <45412598+kengon1315@users.noreply.github.com>
2018-12-01 22:57:34 -0700

    File Uploads

--- script.js
+++ script.js
@@ -10,6 +10,29 @@ $(document).ready(function(){
 					$(this).hide();
 				});
 		});
+		
+	$(".Order").hover(
+		function(){
+			
+			//grab this objects class and animate
+			var id = "#" + $(this).attr("id") + " .OrderSlideUp";
+			$(id).animate({
+				top: 0
+			},{
+				queue: false,
+				duration: 300
+			});
+			
+		}, function(){
+			//make sure all animations are default
+			$(".OrderSlideUp").animate({
+				top: 270
+			}, {
+				queue: false,
+				duration: 300
+			});
+			
+		});
 	
 });
 
